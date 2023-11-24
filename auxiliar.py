255dd26c7639e2bbb76088a7b3b198d140c7e0da
nico190999 <135916321+nico190999@users.noreply.github.com>
2023-11-23 22:32:29 -0300

    Add files via upload

---
+++ auxiliar.py
@@ -0,0 +1,28 @@
+import pygame as pg
+import os
+class Auxiliar:
+    #Flip es para voltear al personaje, lease que mire para el otro lado 
+    @staticmethod
+    def get_surface_from_sprite_sheet(path, columns, rows, flip=False): #Función para obtener cada fotograma del sprite sheet
+        lista = []
+        surface_image = pg.image.load(path)
+        fotograma_width = int(surface_image.get_width()/columns)
+        fotograma_height = int(surface_image.get_height()/rows)
+        for row in range(rows):
+            for column in range(columns):
+                x = column * fotograma_width
+                y = row * fotograma_height
+                surface_fotograma = surface_image.subsurface(x,y,fotograma_width,fotograma_height)
+                if flip:
+                    surface_fotograma = pg.transform.flip(surface_fotograma, True, False)
+                lista.append(surface_fotograma)
+        return lista
+    
+    @staticmethod
+    def get_animation_list_from_folder(path, width, height, flip=False):
+        animation_list = sorted([f for f in os.listdir(path)])
+        animation_list = [pg.image.load(os.path.join(path, img)) for img in animation_list]
+        animation_list = [pg.transform.scale(image, (width, height)) for image in animation_list]
+        if flip:
+            animation_list = [pg.transform.flip(image, True, False) for image in animation_list]
+        return animation_list
---
+++ constantes.py
@@ -0,0 +1,5 @@
+width_window = 1000
+height_window = 600 
+FPS = 60 #Velocidad del juego
+path_image = ""
+DEBUG = True
